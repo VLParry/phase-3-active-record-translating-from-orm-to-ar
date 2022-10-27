3c4b9c96126f0ac797fbfde212ef18d8f39eec2c
Victoria Parry <vlparry@gmail.com>
2022-10-27 14:32:22 -0400

    commit tree aca40d1d6ecf1a264ccdffbc133526dd13936c00

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,21 @@
-class Dog
+ActiveRecord::Base.establish_connection(
+  adapter: "sqlite3",
+  database: "db/test.sqlite"
+)
+
+    sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS dogs (
+    id INTEGER PRIMARY KEY,
+    name TEXT
+  )
+SQL
+ActiveRecord::Base.connection.execute(sql)
+
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+# Have the Student class inherit from ActiveRecord::Base
+class Dog < ActiveRecord::Base
 end
+
+
+
